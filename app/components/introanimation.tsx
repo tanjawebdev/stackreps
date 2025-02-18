dfe2a791dee3a40914dcfe3888c7e9f7b7c8117c
Tanja Kobler <kobler.tanja@gmail.com>
2025-02-18 12:36:41 +0100

    cards spline

--- app/components/introanimation.tsx
+++ app/components/introanimation.tsx
@@ -1,9 +1,26 @@
+"use client"
+
 import styles from '../styles/IntroAnimation.module.scss';
+import {useEffect} from "react";
 
 export default function IntroAnimation() {
+    useEffect(() => {
+        // Load Spline script dynamically
+        const script = document.createElement("script");
+        script.type = "module";
+        script.src = "https://unpkg.com/@splinetool/viewer@1.9.66/build/spline-viewer.js";
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script); // Cleanup on unmount
+        };
+    }, []);
+
   return (
       <section className={styles.introAnimation + " container-fluid"}>
-          <div className={styles.cardAnimation}></div>
+          <div className={styles.cardAnimation}>
+              <spline-viewer url="https://prod.spline.design/Qq0Pcq6S2pPLYNhR/scene.splinecode"></spline-viewer>
+          </div>
           <div className={styles.lightspot + " spot"}></div>
       </section>
   )
